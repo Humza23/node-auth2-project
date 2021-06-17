51095cbeec68d145cffcd08f61267d3aea5be4c1
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-06-17 14:54:43 -0400

    MVP

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,26 +1,13 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-
 const router = require("express").Router();
+const bcrypt = require('bcryptjs')
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
-
 const User = require('../users/users-model')
+const jwt = require('jsonwebtoken')
 
 router.post("/register", validateRoleName, (req, res, next) => {
-  const { username, password } = req.body;
-  const { role_name } = req
-  const hash = bcrypt.hashSync(password, 8)
-
-  User.add({ username, password: hash, role_name })
-    .then(newUser => {
-      res.status(201).json(newUser);
-    })
-    .catch(next)
-})
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
     response:
     status 201
     {
@@ -29,55 +16,58 @@ router.post("/register", validateRoleName, (req, res, next) => {
       "role_name": "angel"
     }
    */
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, 8)
+  User.add({ username, password: hash, role_name })
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      next(err)
+    })
+});
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  if (bcrypt.compareSync(req.body.password, req.user.password)) {
-    const token = tokenBuilder(req.user)
-    res.json({
-      status: 200,
-      message: `${req.user.username} is back!`,
-      token,
-    })
-  } else {
-    next({ status: 401, message: 'Invalid credentials' })
-  }
-});
-
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
     response:
     status 200
     {
       "message": "sue is back!",
       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
     }
-
     The token must expire in one day, and must provide the following information
     in its payload:
-
     {
       "subject"  : 1       // the user_id of the authenticated user
       "username" : "bob"   // the username of the authenticated user
       "role_name": "admin" // the role of the authenticated user
     }
    */
+  if (bcrypt.compareSync(req.body.password, req.user.password)) {
+    const token = buildToken(req.user)
+    res.json({
+      message: `${req.user.username} is back!`,
+      token,
+    })
+  }
+  else {
+    next({ status: 401, message: 'Invalid credentials'})
+  }
+});
 
-function tokenBuilder(user) {
+function buildToken(user) {
   const payload = {
     subject: user.user_id,
-    username: user.username,
     role_name: user.role_name,
+    username: user.username
   }
   const options = {
     expiresIn: '1d',
   }
-  return jwt.sign(
-    payload,
-    JWT_SECRET,
-    options,
-  )
-} 
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,18 +1,8 @@
 const db = require('../../data/db-config.js');
-
 function find() {
-  db.select('u.user_id', 'u.username', 'r.role_name')
-    .from('users as u')
-    .join('roles as r', 'u.role_id', '=', 'r.role_id')
-}
-// select u.user_id, u.username, r.role_name
-// from users as u
-// join roles as r
-// where u.role_id = r.role_id
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
-
     [
       {
         "user_id": 1,
@@ -26,17 +16,15 @@ function find() {
       }
     ]
    */
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'role_name')
+}
 
 function findBy(filter) {
-  db.select('u.user_id', 'u.username', 'u.password', 'r.role_name')
-    .from('users as u')
-    .join('roles as r', 'u.role_id', '=', 'r.role_id')
-    .where(filter)
-}
   /**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
-
     [
       {
         "user_id": 1,
@@ -46,38 +34,37 @@ function findBy(filter) {
       }
     ]
    */
-
-function findById(user_id) {
-  db.select('u.user_id', 'u.username', 'r.role_name')
-  .from('users as u')
-  .join('roles as r', 'u.role_id', '=', 'r.role_id')
-  .where('u.user_id', user_id).first()
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username','password', 'role_name')
+    .where(filter)
 }
 
+function findById(user_id) {
   /**
     You will need to join two tables.
     Resolves to the user with the given user_id.
-
     {
       "user_id": 2,
       "username": "sue",
       "role_name": "instructor"
     }
    */
-
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('users.user_id', user_id).first()
+}
 
 /**
   Creating a user requires a single insert (into users) if the role record with the given
   role_name already exists in the db, or two inserts (into roles and then into users)
   if the given role_name does not exist yet.
-
   When an operation like creating a user involves inserts to several tables,
   we want the operation to succeed or fail as a whole. It would not do to
   insert a new role record and then have the insertion of the user fail.
-
   In situations like these we use transactions: if anything inside the transaction
   fails, all the database changes in it are rolled back.
-
   {
     "user_id": 7,
     "username": "anna",
@@ -100,7 +87,6 @@ async function add({ username, password, role_name }) { // done for you
   })
   return findById(created_user_id)
 }
-
 module.exports = {
   add,
   find,
